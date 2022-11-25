da5d10bea5f2b819cb0d4e14998c3176f25caf2f
4MarkusMarkus <4markusmarkus@gmail.com>
2022-11-25 16:34:12 +0800

    deploy script not working, *.deployed commented out

--- demo-contract/scripts/deploy.js
+++ demo-contract/scripts/deploy.js
@@ -19,7 +19,7 @@ async function main() {
 
     console.log("deploying ssvETH...");
     const ssvETH = await SSVETH.deploy();
-    await ssvETH.deployed();
+    // await ssvETH.deployed();
     // const ssvETH = await ssvETH.attach("0x7a2088a1bFc9d81c55368AE168C2C02570cB814F");
     console.log("ssvETH deployed to:", ssvETH.address)
 
@@ -28,7 +28,7 @@ async function main() {
 
     console.log("deploying staking Pool...");
     const stakingPool = await StakingPoolABI.deploy(whitelist, deposit_contract, withdrawal_creds, ssv_network_contract, ssv_token_address, ssvETH.address, operator_ids);
-    await stakingPool.deployed();
+    // await stakingPool.deployed();
     console.log("staking pool deployed to:", stakingPool.address)
     // tx = await ssvETH.setMinter(stakingpool.address);
     // await tx.wait()
