e75baa3944269f6cca55b7772784b92edb687b29
RohitAudit <rohit@persistence.one>
2023-01-10 11:57:50 +0400

    corrected local ganache-cli options

--- utils/eth_connector.py
+++ utils/eth_connector.py
@@ -5,12 +5,14 @@ import web3
 class EthNode:
     account = None
     eth_node = None
+    local = False
 
     def __init__(self, rpc_url, private_key):
         self.eth_node = Web3(Web3.HTTPProvider(rpc_url))
         if rpc_url == 'http://127.0.0.1:8545':
             w3 = web3.Web3(web3.HTTPProvider(rpc_url))
             # w3.eth.accounts()[0]
+            self.local = True
             self.account = '0xf4F8040E3D9505f7d9F661Ca174435B41e39304D'
         else:
             self.account = self.eth_node.eth.account.privateKeyToAccount(
@@ -22,7 +24,11 @@ class EthNode:
         tx['nonce'] = self.eth_node.eth.get_transaction_count(
             self.account.address)
         tx['gas'] = 8000000
-        # tx['gasPrice'] = self.eth_node.toWei('2', 'gwei')
+        # tx['chainId'] = self.eth_node.eth.chain_id
+        if self.local:
+            tx.pop('maxPriorityFeePerGas')
+            tx.pop('maxFeePerGas')
+        tx['gasPrice'] = self.eth_node.toWei('2', 'gwei')
         signed_tx = self.eth_node.eth.account.sign_transaction(
             tx, self.account.key)
         tx_hash = self.eth_node.eth.send_raw_transaction(
--- utils/ssv_network.py
+++ utils/ssv_network.py
@@ -22,3 +22,59 @@ class SSVNetwork:
 
     def get_network_fee(self):
         return self.contract.functions.getNetworkFee().call()
+
+
+class SSVToken:
+    abi = [{
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }, {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+    ]
+
+    def __init__(self, ssv_address, web3: Web3):
+        self.contract = web3.eth.contract(abi=self.abi, address=ssv_address)
+
+    def get_balance(self, account_address):
+        return self.contract.functions.balanceOf(account_address).call()
+
+    def transfer_token(self, address, amount, account_address):
+        return self.contract.functions.transfer(address, amount).buildTransaction(
+            {"from": account_address, "maxFeePerGas": 10 ** 12})
