7dde515db3fd49227514d34b98db8e4a857b7ead
markoinether <4markusmarkus@gmail.com>
2023-02-22 16:12:20 +0800

    documentation updated

--- frontend/packages/hardhat/scripts/deploy.js
+++ frontend/packages/hardhat/scripts/deploy.js
@@ -4,7 +4,13 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+
+// import { ethers } from "hardhat";
+// const ethers = require("hardhat");
+
 const hre = require("hardhat");
+
+
 
 async function main() {
   const whitelist = "0x123465f75D79AdAEde008E978208cb2Cc28E8B04";
@@ -14,6 +20,7 @@ async function main() {
   const depositContract = "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b";
   const ssvNetworkContract = "0xb9e155e65B5c4D66df28Da8E9a0957f06F11Bc04";
   const ssvTokenAddress = "0x3a9f01091C446bdE031E39ea8354647AFef091E7";
+  const amount = hre.ethers.utils.parseEther("62");
 
   const StakingPool = await hre.ethers.getContractFactory("StakingPool");
   const stakingpool = await StakingPool.deploy(
@@ -27,7 +34,12 @@ async function main() {
 
   await stakingpool.deployed();
 
+
+
   console.log(`StakingPool deployed to ${stakingpool.address}`);
+
+  await stakingpool.stake({value: amount});
+  console.log(`StakingPool funded`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
