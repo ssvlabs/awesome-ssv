4fbec7b3075294cff270143910d31f1ade362536
markoinether <4markusmarkus@gmail.com>
2023-06-21 08:10:19 +0200

    lsd removed

--- frontend/packages/react-app/src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export { default as useDebounce } from "./useDebounce";
-export { default as useLocalStorage } from "./useLocalStorage";
-export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
-export { default as useGasPrice } from "./GasPrice";
-export { default as useTokenPrice } from "./useTokenPrice";
-export * from "./useContractConfig";
